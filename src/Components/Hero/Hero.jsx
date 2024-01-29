e75dcbd04d8a6f0be50134ffa6bfe4a5b8726696
Biolater <yusifovmurad1@gmail.com>
2024-01-29 11:42:44 +0400

    fixed hero section layout

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,39 +1,55 @@
-import { Google, Instagram, Snapchat, X, Pinterest, Reddit, Spotify, Microsoft } from "../../Utilities/Svgs"
+import React from "react";
+import {
+  Google,
+  Instagram,
+  Snapchat,
+  X,
+  Pinterest,
+  Reddit,
+  Spotify,
+  Microsoft,
+} from "../../Utilities/Svgs";
 
 const Hero = () => {
   return (
-    <section id="hero" className="h-screen">
-      <div className="container px-4 pt-24 mx-auto">
-        <div className="hero__wrapper">
-          <h2 className="hero__title text-center text-4.5xl font-extrabold">
-            Empower Your <span className="text-navy">Insight</span>. Discover{" "}
-            <span className="text-bluish">Companies</span>, Stay <span className="text-navy">Informed.</span>
-          </h2>
-          <div className="hero__icons inline-grid gap-6 mt-10 grid-cols-4 items-center">
-            <div className="hero__icon flex justify-center items-center cursor-pointer">
-              <Google />
-            </div>
-            <div className="hero__icon flex justify-center items-center cursor-pointer">
-              <Instagram />
-            </div>
-            <div className="hero__icon flex justify-center items-center cursor-pointer">
-              <Snapchat />
-            </div>
-            <div className="hero__icon flex justify-center items-center cursor-pointer">
-              <X />
-            </div>
-            <div className="hero__icon flex justify-center items-center cursor-pointer">
-              <Pinterest />
-            </div>
-            <div className="hero__icon flex justify-center items-center cursor-pointer">
-              <Reddit />
-            </div>
-            <div className="hero__icon flex justify-center items-center cursor-pointer">
-              <Spotify />
-            </div>
-            <div className="hero__icon flex justify-center items-center cursor-pointer">
-              <Microsoft />
-            </div>
+    <section id="hero" className="min-h-screen pt-24 flex items-center pb-10">
+      <div className="container px-4 mx-auto">
+        <div className="hero__wrapper flex flex-col lg:grid lg:grid-cols-2 lg:items-center lg:gap-7 xl:gap-0 justify-center">
+          <div className="hero__text-content flex flex-col items-center lg:items-start">
+            <h2 className="hero__title text-center lg:text-start text-4.5xl sm:text-5xl sm:max-w-96 sm:mx-auto sm:leading-tight font-extrabold md:max-w-none md:text-6xl md:leading-snug">
+              Empower Your <span className="text-navy">Insight</span>. Discover{" "}
+              <span className="text-bluish">Companies</span>, Stay{" "}
+              <span className="text-navy">Informed.</span>
+            </h2>
+            <button className="bg-bluish transition-transform active:shadow-2xl hover:scale-105 text-white text-lg py-2 font-semibold mt-3 px-4 rounded-2xl">
+              Start Now
+            </button>
+          </div>
+          <div className="hero__icons inline-grid lg:hidden gap-6 mt-10 grid-cols-4 items-center">
+            {[
+              Google,
+              Instagram,
+              Snapchat,
+              X,
+              Pinterest,
+              Reddit,
+              Spotify,
+              Microsoft,
+            ].map((Icon, index) => (
+              <div
+                key={index}
+                className="hero__icon flex justify-center items-center cursor-pointer"
+              >
+                <Icon />
+              </div>
+            ))}
+          </div>
+          <div className="hero-image hidden lg:block lg:w-3/4 ms-auto">
+            <img
+              className="rounded-xl"
+              src="https://prompts.aituts.com/images/illustration/detailed-tech-brand-illustrations/user%20being%20inspired%20by%20the%20possibilities%20of%20an%20app,%20illustration%20for%20a%20tech%20company,%20by%20slack%20and%20dropbox,%20style%20of%20behance%20.webp"
+              alt=""
+            />
           </div>
         </div>
       </div>
